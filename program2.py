797abba55a01569a1f2e2863338927d2f8ecde5c
Ritesh dhote <dhoteritesh66@gmail.com>
2024-09-25 12:56:00 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -17,7 +17,7 @@ def decode_message(s: str, p: str) -> bool:
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             if p[j - 1] == s[i - 1] or p[j - 1] == '?':
-                # If current chars match, or pattern has '?' (match one char)
+               
                 dp[i][j] = dp[i - 1][j - 1]
             elif p[j - 1] == '*':
                 
