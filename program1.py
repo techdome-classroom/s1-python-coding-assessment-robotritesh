0ff3b8e835355de88a7eef6d6de0211fe9b8687b
Ritesh dhote <dhoteritesh66@gmail.com>
2024-09-25 12:55:16 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -8,7 +8,7 @@ class Solution:
             if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == 'W':
                 return
             
-            # Mark the current land cell as visited by turning it into water.
+            
             grid[r][c] = 'W'
             
             
